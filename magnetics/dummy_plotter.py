697f5b191f41381e93625b78fbe65522cd9164a2
Apiwat Wijaikhum <Apiwat.Wijaikhum@egat.co.th>
2024-10-15 15:42:42 +0700

    inital commit

---
+++ magnetics/dummy_plotter.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.plot(x := np.linspace(0, 4*np.pi), np.sin(3*x))
